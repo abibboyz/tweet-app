ba70edbee8345a8d437690e5eeafcc805cbc9d79
Abinash <abi.kh03@gmail.com>
2022-10-16 22:20:46 +1100

    flash message added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   #go to about
-
+  get "about-us", to: "about#index", as: :about
   # Defines the root path route ("/")
   # root "articles"
   root to: "main#index"
